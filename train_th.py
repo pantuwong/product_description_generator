cb122d246d315febaeb7820a89a33258d07908f7
Natapon Pantuwong <pantuwong@gmail.com>
2022-08-17 22:05:01 +0700

    fix train_th

--- train_th.py
+++ train_th.py
@@ -1,53 +1,14 @@
 import pandas as pd
-import torch
-from torch.utils.data import Dataset, random_split
-from transformers import GPT2Tokenizer, TrainingArguments, Trainer, GPT2LMHeadModel
+from padthai import *
 
-torch.manual_seed(42)
+descriptions = pd.read_excel('description_th_dataset_clean.xlsx')['combine'].to_list()
 
-tokenizer = GPT2Tokenizer.from_pretrained('flax-community/gpt2-base-thai', bos_token='<|startoftext|>',
-                                          eos_token='<|endoftext|>', pad_token='<|pad|>')
-model = GPT2LMHeadModel.from_pretrained('flax-community/gpt2-base-thai').cuda()
-model.resize_token_embeddings(len(tokenizer))
-
-descriptions = pd.read_excel('description_th_dataset_clean.xlsx')['combine']
-
-max_length = max([len(tokenizer.encode(description)) for description in descriptions])
-
-class DescriptionDataset(Dataset):
-    def __init__(self, txt_list, tokenizer, max_length):
-        self.input_ids = []
-        self.attn_masks = []
-        self.labels = []
-        for txt in txt_list:
-            encodings_dict = tokenizer('<|startoftext|>' + txt + '<|endoftext|>', truncation=True,
-                                       max_length=max_length, padding="max_length")
-            self.input_ids.append(torch.tensor(encodings_dict['input_ids']))
-            self.attn_masks.append(torch.tensor(encodings_dict['attention_mask']))
-
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, idx):
-        return self.input_ids[idx], self.attn_masks[idx]
-
-dataset = DescriptionDataset(descriptions, tokenizer, max_length=max_length)
-train_size = int(0.9 * len(dataset))
-train_dataset, val_dataset = random_split(dataset, [train_size, len(dataset) - train_size])
-
-import gc
-gc.collect()
-
-torch.cuda.empty_cache()
-
-training_args = TrainingArguments(output_dir='./results_th', num_train_epochs=7, logging_steps=100, save_steps=50000,
-                                  per_device_train_batch_size=1, per_device_eval_batch_size=1,
-                                  warmup_steps=10, weight_decay=0.05, logging_dir='./logs_th', report_to = 'none')
-
-Trainer(model=model,  args=training_args, train_dataset=dataset, 
-        eval_dataset=val_dataset, data_collator=lambda data: {'input_ids': torch.stack([f[0] for f in data]),
-                                                              'attention_mask': torch.stack([f[1] for f in data]),
-                                                              'labels': torch.stack([f[0] for f in data])}).train(resume_from_checkpoint = './results/checkpoint-200000')
-
-
-
+flaxgpt2_model = FlaxGPT2FewShot('./results_th')
+flaxgpt2_model.train(
+    descriptions,
+    logging_dir='./logs_th',
+    num_train_epochs=100,
+    train_size=0.9,
+    batch_size=8,
+    save_every_epochs=False
+)
